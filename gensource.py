a2ab433b1216df1fa400258b1153894e51989b16
Charles Childers <crc@forthworks.com>
2016-05-16 02:22:10 +0000

    step 1 towards Naje source in Markdown

---
+++ gensource.py
@@ -0,0 +1,25 @@
+import os
+
+def extract_from_markdown(name):
+    v = []
+    with open(name) as f:
+        v = f.readlines()
+    s = []
+    fence = False
+    for l in v:
+        l = l.rstrip()
+        if fence == True and l != '````':
+            s.append(l)
+        elif l == '````' and fence == True:
+            fence = False
+        elif l == '````' and fence == False:
+            fence = True
+    return s
+
+
+if __name__ == '__main__':
+    with open('naje.py', 'w') as file:
+        lines = extract_from_markdown('Naje.md')
+        for line in lines:
+            file.write(line)
+            file.write('\n')
