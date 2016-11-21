ccee3387b762495992b330eca3faa1484a924447
Rob Judd <judd@ob-wan.com>
2016-11-21 23:21:21 +1000

    Nga VM now complete and working

--- incoming/pascal/nga.pas
+++ incoming/pascal/nga.pas
@@ -8,8 +8,8 @@ unit nga;
 
 {$mode objfpc}{$H+}
 
-{$define STANDALONE}
-{$define DEBUG}
+//{$define STANDALONE}
+{$MACRO ON}
 
 interface
 
@@ -18,29 +18,25 @@ uses
 
 type Cell = Longint;
 
-type vm_opcode = (
-   VM_NOP, VM_LIT, VM_DUP, VM_DROP, VM_SWAP, VM_PUSH, VM_POP,
-   VM_JUMP, VM_CALL, VM_CCALL, VM_RET, VM_EQ, VM_NEQ, VM_LT,
-   VM_GT, VM_FETCH, VM_STORE, VM_ADD, VM_SUB, VM_MUL, VM_DIVMOD,
-   VM_AND, VM_OR, VM_XOR, VM_SHIFT, VM_ZRET, VM_END
-);
-
 var
-  NUM_OPS : Byte = ord(VM_END) + 1;    // number of opcodes
-  IMAGE_SIZE : Cell = 524288;          // memory image (512k x 32 bits)
   ip, ap, sp : Cell;                   // instruction, address & stack pointers
-  data : array [1..32] of Cell;        // stack depth
-  address : array [1..128] of Cell;    // addresses
-  memory : array [1..524288] of Cell;  // image size
-  TOS, NOS, TOA : Cell;                // top stack, next stack & top address
+  data : array [0..31] of Cell;        // stack depth
+  address : array [0..127] of Cell;    // addresses
+  memory : array [0..524287] of Cell;  // image size
+
+{$DEFINE IMAGE_SIZE:=524288}
+{$DEFINE NUM_OPS:=27}
+{$DEFINE TOS:=data[sp]}
+{$DEFINE NOS:=data[sp-1]}
+{$DEFINE TOA:=address[ap]}
 
 implementation
 
 function ngaLoadImage(imageFile : string) : Cell;
 var
-  f : File;
+  f : File of Cell;
   sr : TSearchRec;
-  fileLen: Cell;
+  fileLen : Cell;
   imageSize : Cell = 0;
 begin
   // Does the file exist?
@@ -48,9 +44,9 @@ begin
   begin
     // Determine number of Cells
     fileLen := sr.Size div sizeof(Cell);
-    // Read the file into memory
+    // Read the file in 4 byte chunks
     Assignfile(f, imageFile);
-    Reset(f);
+    Reset(f, 4);
     try
       BlockRead(f, memory, fileLen, imageSize);
     finally
@@ -64,20 +60,17 @@ begin
 end;
 
 procedure ngaPrepare();
-var
-  i : Cell;
 begin
-  // Initialize pointers
+  // Initialize pointers and arrays
   ip := 0;
   ap := 0;
   sp := 0;
-  // Initialize arrays
-  for i := 1 to length(data) do
-    data[i] := ord(VM_NOP);            // normally zero
-  for i := 1 to length(address) do
-    address[i] := 0;
-  for i := 1 to length(memory) do
-    memory[i] := 0;
+  for ip := 0 to length(data)-1 do
+    data[ip] := 0;                     //ord(VM_NOP);
+  for ip := 0 to length(address)-1 do
+    address[ip] := 0;
+  for ip := 0 to length(memory)-1 do
+    memory[ip] := 0;
 end;
 
 procedure inst_nop();
@@ -219,19 +212,19 @@ end;
 
 procedure inst_add();
 begin
-  NOS := NOS + TOS;
+  NOS += TOS;
   inst_drop();
 end;
 
 procedure inst_sub();
 begin
-  NOS := NOS - TOS;
+  NOS -= TOS;
   inst_drop();
 end;
 
 procedure inst_mul();
 begin
-  NOS := NOS * TOS;
+  NOS *= TOS;
   inst_drop();
 end;
 
@@ -279,6 +272,7 @@ begin
     else
       NOS := x shr y;
   end;
+  inst_drop();
 end;
 
 procedure inst_zret();
@@ -293,57 +287,56 @@ end;
 
 procedure inst_end();
 begin
-  ip := IMAGE_SIZE;
+  ip := IMAGE_SIZE - 1;
 end;
 
 procedure ngaProcessOpcode(opcode : Cell);
 begin
   case opcode of
-    1  : inst_nop();
-    2  : inst_lit();
-    3  : inst_dup();
-    4  : inst_drop();
-    5  : inst_swap();
-    6  : inst_push();
-    7  : inst_pop();
-    8  : inst_jump();
-    9  : inst_call();
-    10 : inst_ccall();
-    11 : inst_ret();
-    12 : inst_eq();
-    13 : inst_neq();
-    14 : inst_lt();
-    15 : inst_gt();
-    16 : inst_fetch();
-    17 : inst_store();
-    18 : inst_add();
-    19 : inst_sub();
-    20 : inst_mul();
-    21 : inst_divmod();
-    22 : inst_and();
-    23 : inst_or();
-    24 : inst_xor();
-    25 : inst_shift();
-    26 : inst_zret();
-    27 : inst_end();
+    0  : inst_nop();
+    1  : inst_lit();
+    2  : inst_dup();
+    3  : inst_drop();
+    4  : inst_swap();
+    5  : inst_push();
+    6  : inst_pop();
+    7  : inst_jump();
+    8  : inst_call();
+    9  : inst_ccall();
+    10 : inst_ret();
+    11 : inst_eq();
+    12 : inst_neq();
+    13 : inst_lt();
+    14 : inst_gt();
+    15 : inst_fetch();
+    16 : inst_store();
+    17 : inst_add();
+    18 : inst_sub();
+    19 : inst_mul();
+    20 : inst_divmod();
+    21 : inst_and();
+    22 : inst_or();
+    23 : inst_xor();
+    24 : inst_shift();
+    25 : inst_zret();
+    26 : inst_end();
   end;
 end;
 
 function ngaValidatePackedOpcodes(opcode : Cell) : Integer;
 var
   raw, current : Cell;
-  valid : Integer = -1;               // value for "true" in Unix-land
-  i : Integer;
+  i : Byte;
 begin
+  result := -1;                        // value for "true" in Unix-land
   raw := opcode;
   for i := 1 to 4 do
   begin
     current := raw and $FF;
     if ((current >= 0) and (current < NUM_OPS)) = false then
-      valid := 0;
+      result := 0;
     raw := raw shr 8;
   end;
-  result := valid;
 end;
 
 procedure ngaProcessPackedOpcodes(opcode : Cell);
@@ -376,7 +369,7 @@ begin
   if size = 0 then
     exit();
 
-  for ip := 1 to size do
+  for ip := 0 to size - 1 do
   begin
     opcode := memory[ip];
     if (ngaValidatePackedOpcodes(opcode)) <> 0 then
@@ -390,11 +383,8 @@ begin
       exit();
     end;
   end;
-{$ifdef DEBUG}
-  // Screen dump addresses for testing
-  for i := 1 to ap do
+  for i := 1 to sp do
     write(format('%d ', [data[i]]));
   writeln();
-{$endif}
 end.
 {$endif}
